d8ef9f790bc52b10b0a3c18e26a18f6ecd6e40f4
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2022-06-21 23:18:12 +0300

    add section design, service, slider, burger, grid

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,64 @@
+console.log("hello");
+console.log("hello world");
+let header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
+
 
 import Splide from '@splidejs/splide';
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	new Splide( '.review-slider', { 
+const revSliderSettings = {
+		type: "loop",
+		gap: "40px",
 		classes :{
 			arrow : 'splide__arrow review-slider__arrow',
 	},
 	pagination:false,
+	
+	breakpoints :{
+		560:{
+			arrows:false,
+			pagination:true,
+			classes: {
+				pagination: 'splide__pagination review-slider__pagination',
+			},
+		},
+	}
+	}
 
-	} ).mount();
-	new Splide( '.review-slider_bg', { 
-		classes :{
-			arrow : 'splide__arrow review-slider__arrow',
-	},
-	pagination:false,
+	new Splide( '#review-slider', revSliderSettings ).mount();
 
-	} ).mount();
+	new Splide( '#author-review', revSliderSettings ).mount();
 
+	// mobile menu
+	const headerBurger = document.querySelector(".header__burger");
+	const headerNav = document.querySelector(".header__nav");
+	const headerClose= document.querySelector("header__burger-close");
+	const headerLink = document.querySelectorAll(".header__menu-link");
+
+
+		headerBurger. addEventListener("click", function (event) {
+		headerBurger.classList.toggle("burger_closed");
+		headerNav.classList.toggle("header__nav_opened");
+		headerClose.classList.toggle("header__burger-close_opened");
+	});
+		
+headerLink.forEach(function(menuLink){
+
+
+		menuLink. addEventListener("click", function (event) {
+			event.preventDefault();
+			menuLink.classList.toggle("_active");
+		});
+		
+})
 })
